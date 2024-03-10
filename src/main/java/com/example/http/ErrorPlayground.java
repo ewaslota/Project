08b2037a8b24c0ca0a33a901c3fc9a1abe7f0cc1
eslota <ewas4852@gmail.com>
2024-03-10 16:46:36 +0100

    Zjazd 6 dzień 2: @RequestMapping

---
+++ src/main/java/com/example/http/ErrorPlayground.java
@@ -0,0 +1,35 @@
+package com.example.http;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErrorPlayground {
+    @GetMapping("/error/1")
+    public ResponseEntity<String> test1() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Princess is in another castle");
+    }
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    @GetMapping("/error/2")
+    public String test2() {
+        return "Princess is in another castle";
+    }
+
+    @GetMapping("/error/3")
+    public String test3() {
+            throw new RuntimeException();
+
+        }
+
+//        @ResponseStatus(HttpStatus.BAD_GATEWAY)
+//        @ExceptionHandler(RuntimeException.class)
+//        public String handle(RuntimeException e) {
+//        return "Ups something went wrong";
+//        }
+
+}
