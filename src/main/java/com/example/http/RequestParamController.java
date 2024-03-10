015e81eca49816fdedfae98afb4bc7c53bbb7b60
eslota <ewas4852@gmail.com>
2024-03-10 11:25:10 +0100

    Zjazd 6 dzień 2: default value

---
+++ src/main/java/com/example/http/RequestParamController.java
@@ -0,0 +1,17 @@
+package com.example.http;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestParamController {
+
+    @GetMapping("/param")
+    public String concat(@RequestParam String left,
+                         @RequestParam(required = false, defaultValue = "") String right) {
+
+        return left.concat(right);
+    }
+}
