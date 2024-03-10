0d405249be93cfa50a808de4ab110472d3f97f7b
eslota <ewas4852@gmail.com>
2024-03-10 09:49:20 +0100

    Initial commit

---
+++ src/main/java/com/example/http/Test.java
@@ -0,0 +1,12 @@
+package com.example.http;
+
+public class Test {
+    private boolean eabled;
+    private String name;
+    private  String hobbies;
+    private Computer computer;
+}
+
+class Computer {
+    private String address;
+}
