84437446438dceccc66c4ed1b8af89f931479ff9
eslota <ewas4852@gmail.com>
2024-03-10 12:40:54 +0100

    Zjazd 6 dzień 2: @RequestBody with objects

--- src/main/java/com/example/http/BodyController.java
+++ src/main/java/com/example/http/BodyController.java
@@ -1,14 +1,51 @@
 package com.example.http;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
+class User {
+    private String name;
+    private String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
 @RestController
 public class BodyController {
 
     @PutMapping(value = "/body/lower", consumes = "text/plain")
     public String lower(@RequestBody String text) {
         return  text.toLowerCase();
+    }
+
+    @PostMapping("/body/save")
+    public String save(@RequestBody User user) {
+        return  user.getName() + " " + user.getSurname() + " saved!";
+    }
+
+    @PutMapping("/body/save")
+    public User put(@RequestBody User user) {
+        return user;
     }
 }
